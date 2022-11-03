2e3d1de8e09a65ea8e38bd00f083670776ca9aac
Tomás Florimo <tflorimo@gmail.com>
2022-11-03 03:37:19 -0300

    agregado de modelos

--- PoetCRM/Models/Paciente.cs
+++ PoetCRM/Models/Paciente.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,31 @@ namespace PoetCRM.Models
 {
     public class Paciente
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPaciente { get; set; }
+
+        [Display(Name = "Nombre del Paciente")]
+        public string NombrePaciente { get; set; }
+
+        [Display(Name = "Numero de Documento")]
+        public string Dni { get; set; }
+
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Display(Name = "Numero de Historial Clinico")]
+        public int HistorialClinico { get; set; }
+
+        [Display(Name = "Correo electronico")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Numero de telefono")]
+        public string Telefono { get; set; }
+
+        [ForeignKey("Plan")]
+        public int IdPlan { get; set; }
+
+        public Plan Plan{ get; set; }
     }
 }
--- PoetCRM/Models/Turno.cs
+++ PoetCRM/Models/Turno.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,24 @@ namespace PoetCRM.Models
 {
     public class Turno
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int IdTurno { get; set; }
+
+        [Display(Name = "Fecha del turno")]
+        public DateTime Fecha { get; set; }
+
+        [ForeignKey("Paciente")]
+        public int IdPaciente { get; set; }
+        public Paciente Paciente { get; set; }
+
+        [ForeignKey("Doctor")]
+        public int IdDoctor { get; set; }
+        public Doctor Doctor { get; set; }
+
+        [ForeignKey("Especialidad")]
+        public int IdEspecialidad { get; set; }
+        public Especialidad Especialidad { get; set; }
     }
 }
