2979af70e2ac049a0cf9e834badd12635dec184b
Tomás Florimo <tflorimo@gmail.com>
2022-11-13 04:51:26 -0300

    agregada selectlist para especialidades en doctor

---
+++ PoetCRM/Controllers/DoctoresController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PoetCRM.Context;
+using PoetCRM.Models;
+
+namespace PoetCRM.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        private readonly PoetCRMDatabaseContext _context;
+
+        public DoctoresController(PoetCRMDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Doctores
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Doctores.ToListAsync());
+        }
+
+        // GET: Doctores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctores
+                .FirstOrDefaultAsync(m => m.IdDoctor == id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
+        // GET: Doctores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdDoctor,Nombre,Apellido,IdEspecialidad")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(doctor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            } 
+
+            List<Especialidad> EspecList = _context.Especialidades.ToList();
+            ViewBag.Especialidades = new SelectList(EspecList, "IdEspecialidad", "NombreEspecialidad");
+
+            var doctor = await _context.Doctores.FindAsync(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdDoctor,Nombre,Apellido,IdEspecialidad")] Doctor doctor)
+        {
+            if (id != doctor.IdDoctor)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(doctor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DoctorExists(doctor.IdDoctor))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctores
+                .FirstOrDefaultAsync(m => m.IdDoctor == id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
+        // POST: Doctores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var doctor = await _context.Doctores.FindAsync(id);
+            _context.Doctores.Remove(doctor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DoctorExists(int id)
+        {
+            return _context.Doctores.Any(e => e.IdDoctor == id);
+        }       
+
+    }
+}
---
+++ PoetCRM/Controllers/EspecialidadesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PoetCRM.Context;
+using PoetCRM.Models;
+
+namespace PoetCRM.Controllers
+{
+    public class EspecialidadesController : Controller
+    {
+        private readonly PoetCRMDatabaseContext _context;
+
+        public EspecialidadesController(PoetCRMDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Especialidades
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Especialidades.ToListAsync());
+        }
+
+        // GET: Especialidades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var especialidad = await _context.Especialidades
+                .FirstOrDefaultAsync(m => m.IdEspecialidad == id);
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(especialidad);
+        }
+
+        // GET: Especialidades/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Especialidades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdEspecialidad,NombreEspecialidad")] Especialidad especialidad)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(especialidad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidad);
+        }
+
+        // GET: Especialidades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var especialidad = await _context.Especialidades.FindAsync(id);
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+            return View(especialidad);
+        }
+
+        // POST: Especialidades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEspecialidad,NombreEspecialidad")] Especialidad especialidad)
+        {
+            if (id != especialidad.IdEspecialidad)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(especialidad);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EspecialidadExists(especialidad.IdEspecialidad))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidad);
+        }
+
+        // GET: Especialidades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var especialidad = await _context.Especialidades
+                .FirstOrDefaultAsync(m => m.IdEspecialidad == id);
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(especialidad);
+        }
+
+        // POST: Especialidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var especialidad = await _context.Especialidades.FindAsync(id);
+            _context.Especialidades.Remove(especialidad);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EspecialidadExists(int id)
+        {
+            return _context.Especialidades.Any(e => e.IdEspecialidad == id);
+        }
+    }
+}
